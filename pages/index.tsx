0469cd2fb1f5c868c5ba6ae399e1feae9b0e534d
jcccc4 <jcsacra4@gmail.com>
2023-04-12 22:29:19 +0800

    Added description

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,10 @@ export default function Home() {
     <>
       <div className={styles.background} />
       <header className={styles.header}>
-        <Image
-          priority
+        <img
           className={styles.logo}
           src="/static/images/logo-light.svg"
-          height={24.61}
-          width={95.76}
+          
           alt="Logo"
         />
         <div className={styles.header_text}>Apply for access</div>
@@ -39,6 +37,24 @@ export default function Home() {
           <div className={styles.description_paragraph}>
             Optimize your products, improve customer satisfaction and stay ahead
             of the competition with our produdct data analytics.
+          </div>
+        </div>
+        <div className={styles.description}>
+          <div className={styles.number}>2</div>
+          <div className={styles.description_item}>Data-driven decisions</div>
+          <div className={styles.description_paragraph}>
+            Make data-driven decisions with our product data analytics. Out
+            AI-genereted reports help you unlock insights hidden in your product
+            data.
+          </div>
+        </div>
+        <div className={styles.description}>
+          <div className={styles.number}>3</div>
+          <div className={styles.description_item}>Always affordable</div>
+          <div className={styles.description_paragraph}>
+            Always affordable pricing that scales with your business. Get
+            top-quality product data analytics services without hidden costs or
+            unexpected fees.
           </div>
         </div>
       </section>
