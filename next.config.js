f243f7b1f36926a12664297057d43fe5c95cc8b4
jcccc4 <jcsacra4@gmail.com>
2023-04-13 18:52:50 +0800

    fixing image load

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
-const withImages = require("next-images");
-module.exports = withImages();
+module.exports = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    formats: ['image/webp','image/png','image/jpeg'],
+  },
+}
